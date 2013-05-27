7a0403ba4cf516bf4b65b6301ecab8c02d244988
Dan Kirkwood <dangogh@gmail.com>
2013-05-27 15:08:40 -0600

    uses idSlice

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/user/newmath"
+       )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("Hello, World! Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
