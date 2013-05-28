a90dd03bc06ef64588b41ee1aba1b900693cb8a9
Dan Kirkwood <dangogh@gmail.com>
2013-05-27 22:38:54 -0600

    improves testing

--- naturally/naturally_test.go
+++ naturally/naturally_test.go
@@ -7,9 +7,16 @@ import (
 )
 
 var testdata = [...]string{"ZZZ", "22", "Z22", "B22", "A0022", "A1"}
-var testexp = [...]string{"", "22", "Z22", "B22", "A0022", "A1"}
+var testexp = [...]string{"", "22", "Z22", "B22", "A0022", "A1", "ZZZ"}
 
 func TestSorts(t *testing.T) {
 	n := naturally.Naturally{testdata[0:]}
 	sort.Sort(n)
+
+	for ii, got := range testdata {
+		exp := testexp[ii]
+		if got != exp {
+			t.Errorf("Got %v; expected %v\n", got, exp)
+		}
+	}
 }
