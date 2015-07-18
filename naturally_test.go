01f42c47622e34885e7c8402d5e87f94db341d54
Dan Kirkwood <dangogh@gmail.com>
2015-07-18 14:18:27 -0600

    expanded test

--- naturally_test.go
+++ naturally_test.go
@@ -1,18 +1,18 @@
 package naturally_test
 
 import (
+	"fmt"
 	"github.com/dangogh/naturally"
 	"sort"
 	"testing"
-	"fmt"
 )
 
 var testdata0 = [...]string{"A1", "A0", "A21", "A11", "A111", "A2"}
 var testexp0 = [...]string{"A0", "A1", "A2", "A11", "A21", "A111"}
 
 // numeric only
-var testdata1 = [...]string{"1", "2", "30", "22", "0", "00", "3"}
-var testexp1 = [...]string{"0", "00", "1", "2", "3", "22", "30"}
+var testdata1 = [...]string{"001", "2", "xyzzy", "30", "22", "0", "00", "3", "1"}
+var testexp1 = [...]string{"0", "00", "1", "001", "2", "3", "22", "30", "xyzzy"}
 
 var testdata2 = [...]string{"A1BA1", "A11AA1", "A2AB0", "B1AA1", "A1AA1"}
 var testexp2 = [...]string{"A1AA1", "A1BA1", "A2AB0", "A11AA1", "B1AA1"}
@@ -26,7 +26,7 @@ func _testSorts(t *testing.T, data, expected []string) {
 		if got != exp {
 			t.Errorf("Got %v; expected %v\n", got, exp)
 			fmt.Printf("Got %v; expected %v\n", got, exp)
-			
+
 		}
 	}
 }
@@ -47,22 +47,21 @@ func TestSortsC(t *testing.T) {
 }
 
 func BenchmarkSortNaturally(b *testing.B) {
-        b.StopTimer()
-        for ii := 0; ii < b.N; ii++ {
-                n := naturally.StringSlice(testdata2[0:])
-                b.StartTimer()
-                sort.Sort(n)
-                b.StopTimer()
-        }
+	b.StopTimer()
+	for ii := 0; ii < b.N; ii++ {
+		n := naturally.StringSlice(testdata2[0:])
+		b.StartTimer()
+		sort.Sort(n)
+		b.StopTimer()
+	}
 }
 
-
 func BenchmarkSort(b *testing.B) {
-        b.StopTimer()
-        for ii := 0; ii < b.N; ii++ {
-                n := sort.StringSlice(testdata2[0:])
-                b.StartTimer()
-                sort.Sort(n)
-                b.StopTimer()
-        }
+	b.StopTimer()
+	for ii := 0; ii < b.N; ii++ {
+		n := sort.StringSlice(testdata2[0:])
+		b.StartTimer()
+		sort.Sort(n)
+		b.StopTimer()
+	}
 }
